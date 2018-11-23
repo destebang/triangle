89bdc1b293bc967206967596c57c84b4c940a1de
Daniel Esteban <danielestebangonzalez83@gmail.com>
2018-11-24 00:41:43 +0100

    Cleaning code

--- src/Domain/Model/TriangleType.php
+++ src/Domain/Model/TriangleType.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tradeshift\Triangle\Domain\Model;
 
-use phpDocumentor\Reflection\Types\Self_;
-
 final class TriangleType
 {
     public const EQUILATERAL = 'EQUILATERAL';
--- src/Domain/Service/TriangleFactory.php
+++ src/Domain/Service/TriangleFactory.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Tradeshift\Triangle\Domain\Service;
 
-use Tradeshift\Triangle\Domain\Model\EquilateralTriangle;
-use Tradeshift\Triangle\Domain\Model\IsoscelesTriangle;
-use Tradeshift\Triangle\Domain\Model\ScaleneTriangle;
 use Tradeshift\Triangle\Domain\Model\Triangle;
 use Tradeshift\Triangle\Domain\Model\TriangleSides;
 use Tradeshift\Triangle\Domain\Model\TriangleType;
@@ -15,31 +12,19 @@ final class TriangleFactory
 {
     public function create(TriangleSides $sides): Triangle
     {
-        $differentSidesLength = $sides->differentLengths();
-        $this->guardAgainstUnsupportedDifferentSidesLength($differentSidesLength);
-        $triangle = $this->differentLengthsToSpecificTriangleMapper()[$differentSidesLength];
+        $specificTriangle = $this->mapDifferentLengthsToSpecificTriangle($sides->differentLengths());
 
-        return new $triangle($sides);
+        return new $specificTriangle($sides);
     }
 
-    private function guardAgainstUnsupportedDifferentSidesLength(int $differentSidesLength): void
+    private function mapDifferentLengthsToSpecificTriangle(int $differentSidesLength): string
     {
-        if (!array_key_exists($differentSidesLength, $this->differentLengthsToSpecificTriangleMapper())) {
-            throw new \RuntimeException("There has been a problem calculating the triangle type");
-        }
-    }
-
-    private function differentLengthsToSpecificTriangleMapper(): array
-    {
-        $map = [];
-
-        /**
-         * @var Triangle $className
-         */
-        foreach (TriangleType::types() as $type => $className) {
-            $map[$className::differentLengthsForType()] = $className;
+        foreach (TriangleType::types() as $type => $triangleClassName) {
+            if ($triangleClassName::differentLengthsForType() === $differentSidesLength) {
+                return $triangleClassName;
+            }
         }
 
-        return $map;
+        throw new \RuntimeException("There has been a problem calculating the triangle type");
     }
 }
